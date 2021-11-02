36ec7d981fb605601287af03bd9525358fda5900
Omar <opace88@gmail.com>
2021-11-02 07:44:18 +0400

    Rename vespa to vespaForm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Redirect, Route, Switch } from "react-router";
-import "./App.css";
 import Navbar from "./components/common/navbar";
 import Vespe from "./components/vespe";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
-import VespaForm from "./components/vespa";
+import VespaForm from "./components/vespaForm";
+import "./App.css";
 
 function App() {
   const links = [
