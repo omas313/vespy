027ea384e355801471a9d471227e6f3d25acf668
Omar <opace88@gmail.com>
2021-11-02 07:07:26 +0400

    Add Routing with react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 import "normalize.css/normalize.css";
 import "./index.css";
@@ -10,7 +11,9 @@ import "milligram/dist/milligram.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
