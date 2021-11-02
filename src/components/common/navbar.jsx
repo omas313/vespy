22172871a555c64d0b606a81190fc25a71d06500
Omar <opace88@gmail.com>
2021-11-02 17:58:02 +0400

    Beautified navbar and added login link

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,33 +1,44 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = ({ brand, links }) => {
   const navStyles = {
     display: "flex",
+    padding: "1rem 2rem",
+    alignItems: "center",
+    background: "#223",
   };
 
   const linkContainerStyles = {
     marginLeft: "2rem",
+    width: "100vw",
+  };
+
+  const listStyles = {
+    marginBottom: "0",
   };
 
   const listItemStyles = {
     display: "inline-block",
     listStyle: "none",
-    marginLeft: "1rem",
+    margin: "0 1rem",
   };
 
   return (
     <nav style={navStyles}>
       <div>
-        <NavLink to="/">{brand}</NavLink>
+        <Link to="/">{brand}</Link>
       </div>
       <div style={linkContainerStyles}>
-        <ul>
+        <ul style={listStyles}>
           {links.map(l => (
             <li style={listItemStyles} key={l.url + l.label}>
               <NavLink to={l.url}>{l.label}</NavLink>
             </li>
           ))}
         </ul>
+      </div>
+      <div>
+        <Link to="/login">Login</Link>
       </div>
     </nav>
   );
