93d5c3727ecab52f0bd7e290fc9e77159a231b03
Omar <opace88@gmail.com>
2021-11-03 18:07:01 +0400

    Modify input component to use rest operator

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,18 +1,9 @@
-const Input = ({ name, label, type, value, placeholder, error, onChange }) => {
-  return (
-    <>
-      {label && <label htmlFor={name}>{label}</label>}
-      <input
-        type={type}
-        id={name}
-        name={name}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-      />
-      {error && <p>{error}</p>}
-    </>
-  );
-};
+const Input = ({ name, label, error, ...rest }) => (
+  <>
+    {label && <label htmlFor={name}>{label}</label>}
+    <input id={name} name={name} {...rest} />
+    {error && <p>{error}</p>}
+  </>
+);
 
 export default Input;
